8e7d600969b5e581962a91744c245ce1cbee3d62
Oludare Samuel <138253128+Dhapor@users.noreply.github.com>
2024-04-27 10:41:49 +0100

    Update Cavista_diabetes_prediction.py

--- Cavista_diabetes_prediction.py
+++ Cavista_diabetes_prediction.py
@@ -222,18 +222,18 @@ if selected_page == "Modeling":
             input_variables[i] = LabelEncoder().fit_transform(input_variables[i])
 
 
-       if patient_name:
-            if st.button('Press To Predict'):
-                st.markdown("<h4 style='color: #2B2A4C; text-align: left; font-family: montserrat;'>Model Report</h4>", unsafe_allow_html=True)
-                predicted = model.predict(input_variables)
-                st.toast('Predicted Successfully')
-                st.image('check icon.png', width=100)
-                if predicted >= 0.5:
-                    st.error(f"{patient_name} you may be at high risk of having Diabetes!")
-                else:
-                    st.success(f"{patient_name} is at low risk of Diabetes")
-        else:
-            st.warning("Please enter the patient's name.")
+   if patient_name:
+        if st.button('Press To Predict'):
+            st.markdown("<h4 style='color: #2B2A4C; text-align: left; font-family: montserrat;'>Model Report</h4>", unsafe_allow_html=True)
+            predicted = model.predict(input_variables)
+            st.toast('Predicted Successfully')
+            st.image('check icon.png', width=100)
+            if predicted >= 0.5:
+                st.error(f"{patient_name} you may be at high risk of having Diabetes!")
+            else:
+                st.success(f"{patient_name} is at low risk of Diabetes")
+    else:
+        st.warning("Please enter the patient's name.")
     
 
     st.markdown("<h8 style = 'color: #2B2A4C; text-align: LEFT; font-family:montserrat'>DIABETES PREDICTION MODEL BUILT BY ORPHEUS SNIPERS </h8>",unsafe_allow_html=True)
